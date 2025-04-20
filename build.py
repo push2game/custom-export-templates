4007378b2a87e46302ee800543b4609b1879554d
Minh Vương <hardingadonis@gmail.com>
2025-04-20 13:45:31 +0700

    feat: add support for Android builds in build_godot function

--- build.py
+++ build.py
@@ -88,8 +88,11 @@ build_args = {
 }
 
 
-def build_godot(mode):
+def build_godot(mode, isAndroid):
     args = " ".join(build_args.get(mode, []))
+
+    if isAndroid:
+        args.append("platform=android arch=arm64 generate_apk=yes")
 
     scons_command = f"scons {args}"
 
@@ -108,8 +111,17 @@ if __name__ == "__main__":
         help="Debug or Release mode, value (``debug`` or ``release``)",
         required=True,
     )
+    parse_args.add_argument(
+        "-a",
+        "--android",
+        type=bool,
+        help="Build for Android, value (``True`` or ``False``)",
+        default=False,
+        required=False,
+    )
 
     args = parser.parse_args()
     mode = args.mode
+    isAndroid = args.android
 
-    build_godot(mode)
+    build_godot(mode, isAndroid)
